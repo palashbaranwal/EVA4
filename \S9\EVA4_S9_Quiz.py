d5b186752b63d7d613bcde32e3a8ed5ef3af91cb
palashbaranwal <palash.baranwal11@gmail.com>
2020-03-18 01:15:02 +0530

    Created using Colaboratory

---
+++ \S9\EVA4_S9_Quiz.py
@@ -0,0 +1,69 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "EVA4_S9",
+      "provenance": [],
+      "collapsed_sections": [],
+      "authorship_tag": "ABX9TyMLgWbLki8CbFk18Xf8pD0E",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/palashbaranwal/EVA4/blob/master/%5CS9%5CEVA4_S9_Quiz.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "1ZvdLVflBAXW",
+        "colab_type": "code",
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 34
+        },
+        "outputId": "b7e891a2-2154-4d50-a6e5-37c882f7f9d6"
+      },
+      "source": [
+        "from datetime import datetime \n",
+        "\n",
+        "print(\"Current Date/Time: \", datetime.now())"
+      ],
+      "execution_count": 3,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "text": [
+            "Current Date/Time:  2020-03-17 19:43:43.224590\n"
+          ],
+          "name": "stdout"
+        }
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "ARIw8umfBX43",
+        "colab_type": "code",
+        "colab": {}
+      },
+      "source": [
+        ""
+      ],
+      "execution_count": 0,
+      "outputs": []
+    }
+  ]
+}
